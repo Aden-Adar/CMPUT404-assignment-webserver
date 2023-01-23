508ea553bbabd3a90e620e3a1867df555a3198ea
Aden-Adar <77550086+Aden-Adar@users.noreply.github.com>
2023-01-23 15:14:59 -0700

    my webserver

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
-
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+import os
+# Copyright 2022 Aden Adar
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +28,54 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.result = self.data.split(b"\r\n")
+        for i in self.result:
+            print(i.decode("utf-8"), end="\n")
+        print()
+
+        # Read request type
+        request_type = self.result[0].split(b" ")[0].decode("utf-8")
+        request_path = self.result[0].split(b" ")[1].decode("utf-8")
+        location = os.path.join(os.path.abspath(os.path.abspath("./www") + request_path))
+        print("Request type: " + request_type)
+        print("Request path: " + request_path)
+        print("Location: " + location)
+        is_subpath = location.startswith(os.path.abspath("./www"))
+
+        # Checks for valid HTTP method
+        if request_type != "GET":
+            response = "HTTP/1.1 405 Method Not Allowed\n\n"
+            self.request.sendall(response.encode())
+            return
+
+        if os.path.isdir(location):
+            if request_path[-1] != "/":
+                response = "HTTP/1.1 301 Moved Permanently\r\nLocation: " + self.server.server_address[0] + ":" + str(self.server.server_address[1]) + request_path + "/\n\n"
+                self.request.sendall(response.encode())
+                return
+
+            if "index.html" in os.listdir(location):
+                location = os.path.join(location, "index.html")
+
+        if os.path.isfile(location) and is_subpath:
+            fin = open(location)
+            content = fin.read()
+            fin.close()
+        else:
+            response = "HTTP/1.1 404 Not Found\n\n"
+            self.request.sendall(response.encode())
+            return
+
+        if location.split('.')[-1] == "html":
+            content_type = "text/html"
+        else:
+            content_type = "text/css"
+        response = 'HTTP/1.1 200 OK\r\n' + "Content-Type: " + content_type + "\n\n" + content
+        self.request.sendall(response.encode())
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
@@ -44,3 +87,7 @@ if __name__ == "__main__":
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
+
+# REFERENCE
+# https://www.codementor.io/@joaojonesventura/building-a-basic-http-server-from-scratch-in-python-1cedkg0842
